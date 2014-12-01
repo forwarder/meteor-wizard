f9bf4ac1b9b73822dbfac40ebc2e4cab648c4535
Eelco Wiersma <eelco@pagebakers.nl>
2014-12-01 16:22:04 +0100

    Added git url

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   name: 'forwarder:autoform-wizard',
   summary: 'A wizard component for AutoForm.',
-  version: '0.3.2'
+  version: '0.3.3',
+  git: 'https://github.com/forwarder/meteor-wizard.git'
 });
 
 Package.onUse(function(api) {
